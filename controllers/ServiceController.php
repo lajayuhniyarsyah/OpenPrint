5fd929d7ec1b2c99d1f26d7ebdcefaa76a60b98d
La Jayuhni Yarsyah <lajayuhni@gmail.com>
2015-07-01 10:18:59 +0700

    Invoice CSV Export Out Invoice

--- controllers/ServiceController.php
+++ controllers/ServiceController.php
@@ -214,13 +214,7 @@ class ServiceController extends Controller{
 				["FAPR","PT SINCHAN","JL PAHLAWAN BERTOPENG BLOK MATAHARI NO.11, KIOTO RT: 1 RW: 14 JAKARTA","","","",""],
 				["OF","0487060880","Secondary Board ESAB Caddy TM A 34","10000000","1.0","10000000","0.0","10000000","1000000.0","0","0.0"]
 			];
-			/*echo '<br/>'.count($tes[0]).'-';
-			echo count($tes[1]).'-';
-			echo count($tes[2]).'-';*/
-
-			/*fputcsv($output, $tes[0]);
-			fputcsv($output, $tes[1]);
-			fputcsv($output, $tes[2]);*/
+			
 			
 			fclose($output);
 			Yii::$app->end();
@@ -251,145 +245,290 @@ class ServiceController extends Controller{
 				continue;
 			}
 
-			$rate = ($inv['currency_id']==13 ? 1:$inv['pajak']);
-			$all_tax_code = substr($inv['faktur_pajak_no'], 0,3);
-			$tax_code_type =  substr($inv['faktur_pajak_no'], 0,2);
-			$tax_replace_code = 0;
-			$tax_mod = $all_tax_code % 10;
-			if($tax_mod==1){
-				// faktur pajak pengganti
-				$tax_replace_code = 1;
-			}
-
-			$pure_faktur_code = substr(preg_replace('/[\s\W]+/', '', $inv['faktur_pajak_no']),3,strlen(preg_replace('/[\s\W]+/', '', $inv['faktur_pajak_no'])));
-			$exp_tax_date = explode('-', $inv['date_invoice']);
-			$tax_date = \DateTime::createFromFormat('Y-m-d',$inv['date_invoice']);
-			$iAddr = $inv['partner']['street'].'\r\n'.$inv['partner']['street2'].' '.$inv['partner']['city'].', '.(isset($inv['partner']['state']->name) ? $inv['partner']['state']->name:'').($inv['partner']['zip'] ? ' - '.$inv['partner']['zip']:"");
-			// "FK","09","0","0011578000001","6","2015","18/06/2015","010621191092000","INDOCEMENT TUNGGAL PRAKARSA TBK PT","Wisma Indocement Lt. 13   Blok - No.- RT:- RW:- Kel.- Kec.- Kota/Kab.- - 12910","56750000","5675000","0","","0","0","0","0","78049887/SBM/V/2015"
-			$res[$inv['id']]['fk'] = [
-				'FK', #FK
-				$tax_code_type, #KD JENIS TRANSAKSI
-				$tax_replace_code, #FG PENGGANTI
-				$pure_faktur_code, #NOMOR FAKTUR
-				$tax_date->format('n'), #MASA PAJAK
-				$tax_date->format('Y'), #TAHUN PAJAK
-				$tax_date->format('d/m/Y'), #full tax date dd/mm/yyyy
-				preg_replace('/[\s\W]+/', '', $inv['partner']['npwp']), #customer npwp
-				$inv['partner']['name'], #customer name
-				$iAddr, #invoice address
-				$this->convertIdr($inv['amount_untaxed'],$rate),#jumlah dpp,
-				$this->convertIdr($inv['amount_tax'],$rate),#jumlah PPN
-				'0', #Jumlah PPNBM
-				'',#id keterangan tambahan
-				($inv['payment_for']=='dp' ? '1':($inv['payment_for']=='completion' ? '2':'0')),#fg uang muka
-				$this->convertIdr($inv['amount_untaxed'],$rate),#uang muka dpp
-				$this->convertIdr($inv['amount_tax'],$rate),#uang muka ppn
-				'0',#uang muka ppnbm, 0 karena tidak ada PPNBM (fix)
-				$inv['origin'],#referensi
-				
-			];
-
-			// "FAPR","PT SINCHAN","JL PAHLAWAN BERTOPENG BLOK MATAHARI NO.11, KIOTO RT: 1 RW: 14 JAKARTA",,,,
-			$res[$inv['id']]['fapr'] = [
-				'FAPR',
-				'SUPRABAKTI MANDIRI, PT',
-				'Jl. Danau Sunter Utara Blok. A No. 9 Tanjung Priok - Jakarta Utara 14350',
-				'','','',''
-			];
-
-
-			// EACH LINE ITEM
-			if($inv['payment_for']){
-				// IF PAYMENT FOR DP OR COMPLETION THEN ALL SALE ORDER ITEM WILL BE RENDERED
-				// check from sale order invoice
-				$soRel = \app\models\SaleOrderInvoiceRel::find()->where(['invoice_id'=>$inv['id']])->with(['order','order.saleOrderLines','order.saleOrderLines.product'])->asArray()->one();
-				
-				if($soRel)
-				{
-					$soInv = $soRel['order'];
-				}else
-				{
-					// check if invoice made from picking
-					if(count($inv['stockPickings'])>0){
-						// if do
-						$sid = [];
-						$soid = null;
-						foreach($inv['stockPickings'] as $no=>$pick):
-							$sid[$pick['sale_id']] = $no++;
-							$soid = $pick['sale_id'];
-						endforeach;
-						if(count($sid)==1){
-							$soInv = \app\models\SaleOrder::find()->where(['id'=>$soid])->with(['saleOrderLines','saleOrderLines.product'])->asArray()->one();
-
-						}else{
-							foreach($inv['accountInvoiceLines'] as $item):
-								$res[$inv['id']]['of'][] = [
-									'OF', #of
-									(string)$item['product']['default_code'],#product code
-									(string)$item['product']['name_template'],#product name
-									$this->convertIdr($item['price_unit'],$rate), #harga satuan
-									(float)$item['quantity'], #jumlah barang
-									$this->convertIdr($item['price_subtotal'],$rate), #harga total
-									$this->convertIdr($item['discount'],$rate), #diskon
-									$this->convertIdr($item['price_subtotal'],$rate), #dpp
-									$this->convertIdr(($item['price_subtotal']*0.1),$rate), #ppn,
-									0, #TARIF PPNBM
-									'0.0' #ppnbm
-								];
-
-							endforeach;
-							$soInv = false;
-						}
-						
-					}
+			if($inv['type']=='out_invoice'){
+				$rate = ($inv['currency_id']==13 ? 1:$inv['pajak']);
+				$all_tax_code = substr($inv['faktur_pajak_no'], 0,3);
+				$tax_code_type =  substr($inv['faktur_pajak_no'], 0,2);
+				$tax_replace_code = 0;
+				$tax_mod = $all_tax_code % 10;
+				if($tax_mod==1){
+					// faktur pajak pengganti
+					$tax_replace_code = 1;
 				}
-				if($soInv):
-					// if founded
-					// override res fk
-					$res[$inv['id']]['fk'][10] = $this->convertIdr($soInv['amount_untaxed'],$rate);
-					$res[$inv['id']]['fk'][11] = $this->convertIdr($soInv['amount_total'],$rate);
 
-					foreach($soInv['saleOrderLines'] as $soLine):
+				$pure_faktur_code = substr(preg_replace('/[\s\W]+/', '', $inv['faktur_pajak_no']),3,strlen(preg_replace('/[\s\W]+/', '', $inv['faktur_pajak_no'])));
+				$exp_tax_date = explode('-', $inv['date_invoice']);
+				$tax_date = \DateTime::createFromFormat('Y-m-d',$inv['date_invoice']);
+				$iAddr = $inv['partner']['street'].'\r\n'.$inv['partner']['street2'].' '.$inv['partner']['city'].', '.(isset($inv['partner']['state']->name) ? $inv['partner']['state']->name:'').($inv['partner']['zip'] ? ' - '.$inv['partner']['zip']:"");
+				// "FK","09","0","0011578000001","6","2015","18/06/2015","010621191092000","INDOCEMENT TUNGGAL PRAKARSA TBK PT","Wisma Indocement Lt. 13   Blok - No.- RT:- RW:- Kel.- Kec.- Kota/Kab.- - 12910","56750000","5675000","0","","0","0","0","0","78049887/SBM/V/2015"
+				$res[$inv['id']]['fk'] = [
+					'FK', #FK
+					$tax_code_type, #KD JENIS TRANSAKSI
+					$tax_replace_code, #FG PENGGANTI
+					$pure_faktur_code, #NOMOR FAKTUR
+					$tax_date->format('n'), #MASA PAJAK
+					$tax_date->format('Y'), #TAHUN PAJAK
+					$tax_date->format('d/m/Y'), #full tax date dd/mm/yyyy
+					preg_replace('/[\s\W]+/', '', $inv['partner']['npwp']), #customer npwp
+					$inv['partner']['name'], #customer name
+					$iAddr, #invoice address
+					$this->convertIdr($inv['amount_untaxed'],$rate),#jumlah dpp,
+					$this->convertIdr($inv['amount_tax'],$rate),#jumlah PPN
+					'0', #Jumlah PPNBM
+					'',#id keterangan tambahan
+					($inv['payment_for']=='dp' ? '1':($inv['payment_for']=='completion' ? '2':'0')),#fg uang muka
+					$this->convertIdr($inv['amount_untaxed'],$rate),#uang muka dpp
+					$this->convertIdr($inv['amount_tax'],$rate),#uang muka ppn
+					'0',#uang muka ppnbm, 0 karena tidak ada PPNBM (fix)
+					$inv['origin'],#referensi
+					
+				];
+
+				// "FAPR","PT SINCHAN","JL PAHLAWAN BERTOPENG BLOK MATAHARI NO.11, KIOTO RT: 1 RW: 14 JAKARTA",,,,
+				$res[$inv['id']]['fapr'] = [
+					'FAPR',
+					'SUPRABAKTI MANDIRI, PT',
+					'Jl. Danau Sunter Utara Blok. A No. 9 Tanjung Priok - Jakarta Utara 14350',
+					'','','',''
+				];
+
+
+				// EACH LINE ITEM
+				if($inv['payment_for']){
+					// IF PAYMENT FOR DP OR COMPLETION THEN ALL SALE ORDER ITEM WILL BE RENDERED
+					// check from sale order invoice
+					$soRel = \app\models\SaleOrderInvoiceRel::find()->where(['invoice_id'=>$inv['id']])->with(['order','order.saleOrderLines','order.saleOrderLines.product'])->asArray()->one();
+					
+					if($soRel)
+					{
+						$soInv = $soRel['order'];
+					}else
+					{
+						// check if invoice made from picking
+						if(count($inv['stockPickings'])>0){
+							// if do
+							$sid = [];
+							$soid = null;
+							foreach($inv['stockPickings'] as $no=>$pick):
+								$sid[$pick['sale_id']] = $no++;
+								$soid = $pick['sale_id'];
+							endforeach;
+							if(count($sid)==1){
+								$soInv = \app\models\SaleOrder::find()->where(['id'=>$soid])->with(['saleOrderLines','saleOrderLines.product'])->asArray()->one();
+
+							}else{
+								foreach($inv['accountInvoiceLines'] as $item):
+									$res[$inv['id']]['of'][] = [
+										'OF', #of
+										(string)$item['product']['default_code'],#product code
+										(string)$item['product']['name_template'],#product name
+										$this->convertIdr($item['price_unit'],$rate), #harga satuan
+										(float)$item['quantity'], #jumlah barang
+										$this->convertIdr($item['price_subtotal'],$rate), #harga total
+										$this->convertIdr($item['discount'],$rate), #diskon
+										$this->convertIdr($item['price_subtotal'],$rate), #dpp
+										$this->convertIdr(($item['price_subtotal']*0.1),$rate), #ppn,
+										0, #TARIF PPNBM
+										'0.0' #ppnbm
+									];
+
+								endforeach;
+								$soInv = false;
+							}
+							
+						}
+					}
+					if($soInv):
+						// if founded
+						// override res fk
+						$res[$inv['id']]['fk'][10] = $this->convertIdr($soInv['amount_untaxed'],$rate);
+						$res[$inv['id']]['fk'][11] = $this->convertIdr($soInv['amount_total'],$rate);
+
+						foreach($soInv['saleOrderLines'] as $soLine):
+							$res[$inv['id']]['of'][] = [
+								'OF',
+								(string)$soLine['product']['default_code'],#product code
+								(string)$soLine['product']['name_template'],#product name
+								$this->convertIdr($soLine['price_unit'],$rate), #HARGA SATUAN
+								(float)$soLine['product_uom_qty'], #qty
+								$this->convertIdr(($soLine['price_unit']*$soLine['product_uom_qty']),$rate), #HARGA TOTAL
+								$this->convertIdr($soLine['discount'],$rate), #DISKON
+								$this->convertIdr(($soLine['price_unit']*$soLine['product_uom_qty']),$rate), #dpp
+								$this->convertIdr((($soLine['price_unit']*$soLine['product_uom_qty'])*0.1),$rate), #ppn,
+								0, #TARIF PPNBM
+								'0.0' #ppnbm
+
+							];	
+
+						endforeach;
+					endif;
+				}else{
+					foreach($inv['accountInvoiceLines'] as $item):
 						$res[$inv['id']]['of'][] = [
-							'OF',
-							(string)$soLine['product']['default_code'],#product code
-							(string)$soLine['product']['name_template'],#product name
-							$this->convertIdr($soLine['price_unit'],$rate), #HARGA SATUAN
-							(float)$soLine['product_uom_qty'], #qty
-							$this->convertIdr(($soLine['price_unit']*$soLine['product_uom_qty']),$rate), #HARGA TOTAL
-							$this->convertIdr($soLine['discount'],$rate), #DISKON
-							$this->convertIdr(($soLine['price_unit']*$soLine['product_uom_qty']),$rate), #dpp
-							$this->convertIdr((($soLine['price_unit']*$soLine['product_uom_qty'])*0.1),$rate), #ppn,
-							0, #TARIF PPNBM
-							'0.0' #ppnbm
+							'OF', #of
+							(string)$item['product']['default_code'],#product code
+							(string)$item['product']['name_template'],#product name
+							$this->convertIdr($item['price_unit'],$rate), #harga satuan
+							(float)$item['quantity'], #jumlah barang
+							$this->convertIdr($item['price_subtotal'],$rate), #harga total
+							$this->convertIdr($item['discount'],$rate), #diskon
+							$this->convertIdr($item['price_subtotal'],$rate), #dpp
+							$this->convertIdr(($item['price_subtotal']*0.1),$rate), #ppn,
+							'0', #ppnbm,
+							'0.0'
 
-						];	
-
+						];
+						// echo $item['price_subtotal'].'\\';
 					endforeach;
-				endif;
-			}else{
-				foreach($inv['accountInvoiceLines'] as $item):
-					$res[$inv['id']]['of'][] = [
-						'OF', #of
-						(string)$item['product']['default_code'],#product code
-						(string)$item['product']['name_template'],#product name
-						$this->convertIdr($item['price_unit'],$rate), #harga satuan
-						(float)$item['quantity'], #jumlah barang
-						$this->convertIdr($item['price_subtotal'],$rate), #harga total
-						$this->convertIdr($item['discount'],$rate), #diskon
-						$this->convertIdr($item['price_subtotal'],$rate), #dpp
-						$this->convertIdr(($item['price_subtotal']*0.1),$rate), #ppn,
-						'0', #ppnbm,
-						'0.0'
-
-					];
-					// echo $item['price_subtotal'].'\\';
-				endforeach;
+				}
+			}elseif($inv['type']=='in_invoice'){
+				$res = $this->prepareIn();
 			}
 			
 		}
 		// \yii\helpers\VarDumper::dump($res);
 		return $res;
 	}
+
+	private function prepareOut($inv){
+		$res = [];
+		$rate = ($inv['currency_id']==13 ? 1:$inv['pajak']);
+		$all_tax_code = substr($inv['faktur_pajak_no'], 0,3);
+		$tax_code_type =  substr($inv['faktur_pajak_no'], 0,2);
+		$tax_replace_code = 0;
+		$tax_mod = $all_tax_code % 10;
+		if($tax_mod==1){
+			// faktur pajak pengganti
+			$tax_replace_code = 1;
+		}
+
+		$pure_faktur_code = substr(preg_replace('/[\s\W]+/', '', $inv['faktur_pajak_no']),3,strlen(preg_replace('/[\s\W]+/', '', $inv['faktur_pajak_no'])));
+		$exp_tax_date = explode('-', $inv['date_invoice']);
+		$tax_date = \DateTime::createFromFormat('Y-m-d',$inv['date_invoice']);
+		$iAddr = $inv['partner']['street'].'\r\n'.$inv['partner']['street2'].' '.$inv['partner']['city'].', '.(isset($inv['partner']['state']->name) ? $inv['partner']['state']->name:'').($inv['partner']['zip'] ? ' - '.$inv['partner']['zip']:"");
+		// "FK","09","0","0011578000001","6","2015","18/06/2015","010621191092000","INDOCEMENT TUNGGAL PRAKARSA TBK PT","Wisma Indocement Lt. 13   Blok - No.- RT:- RW:- Kel.- Kec.- Kota/Kab.- - 12910","56750000","5675000","0","","0","0","0","0","78049887/SBM/V/2015"
+		$res[$inv['id']]['fk'] = [
+			'FK', #FK
+			$tax_code_type, #KD JENIS TRANSAKSI
+			$tax_replace_code, #FG PENGGANTI
+			$pure_faktur_code, #NOMOR FAKTUR
+			$tax_date->format('n'), #MASA PAJAK
+			$tax_date->format('Y'), #TAHUN PAJAK
+			$tax_date->format('d/m/Y'), #full tax date dd/mm/yyyy
+			preg_replace('/[\s\W]+/', '', $inv['partner']['npwp']), #customer npwp
+			$inv['partner']['name'], #customer name
+			$iAddr, #invoice address
+			$this->convertIdr($inv['amount_untaxed'],$rate),#jumlah dpp,
+			$this->convertIdr($inv['amount_tax'],$rate),#jumlah PPN
+			'0', #Jumlah PPNBM
+			'',#id keterangan tambahan
+			($inv['payment_for']=='dp' ? '1':($inv['payment_for']=='completion' ? '2':'0')),#fg uang muka
+			$this->convertIdr($inv['amount_untaxed'],$rate),#uang muka dpp
+			$this->convertIdr($inv['amount_tax'],$rate),#uang muka ppn
+			'0',#uang muka ppnbm, 0 karena tidak ada PPNBM (fix)
+			$inv['origin'],#referensi
+			
+		];
+
+		// "FAPR","PT SINCHAN","JL PAHLAWAN BERTOPENG BLOK MATAHARI NO.11, KIOTO RT: 1 RW: 14 JAKARTA",,,,
+		$res[$inv['id']]['fapr'] = [
+			'FAPR',
+			'SUPRABAKTI MANDIRI, PT',
+			'Jl. Danau Sunter Utara Blok. A No. 9 Tanjung Priok - Jakarta Utara 14350',
+			'','','',''
+		];
+
+
+		// EACH LINE ITEM
+		if($inv['payment_for']){
+			// IF PAYMENT FOR DP OR COMPLETION THEN ALL SALE ORDER ITEM WILL BE RENDERED
+			// check from sale order invoice
+			$soRel = \app\models\SaleOrderInvoiceRel::find()->where(['invoice_id'=>$inv['id']])->with(['order','order.saleOrderLines','order.saleOrderLines.product'])->asArray()->one();
+			
+			if($soRel)
+			{
+				$soInv = $soRel['order'];
+			}else
+			{
+				// check if invoice made from picking
+				if(count($inv['stockPickings'])>0){
+					// if do
+					$sid = [];
+					$soid = null;
+					foreach($inv['stockPickings'] as $no=>$pick):
+						$sid[$pick['sale_id']] = $no++;
+						$soid = $pick['sale_id'];
+					endforeach;
+					if(count($sid)==1){
+						$soInv = \app\models\SaleOrder::find()->where(['id'=>$soid])->with(['saleOrderLines','saleOrderLines.product'])->asArray()->one();
+
+					}else{
+						foreach($inv['accountInvoiceLines'] as $item):
+							$res[$inv['id']]['of'][] = [
+								'OF', #of
+								(string)$item['product']['default_code'],#product code
+								(string)$item['product']['name_template'],#product name
+								$this->convertIdr($item['price_unit'],$rate), #harga satuan
+								(float)$item['quantity'], #jumlah barang
+								$this->convertIdr($item['price_subtotal'],$rate), #harga total
+								$this->convertIdr($item['discount'],$rate), #diskon
+								$this->convertIdr($item['price_subtotal'],$rate), #dpp
+								$this->convertIdr(($item['price_subtotal']*0.1),$rate), #ppn,
+								0, #TARIF PPNBM
+								'0.0' #ppnbm
+							];
+
+						endforeach;
+						$soInv = false;
+					}
+					
+				}
+			}
+			if($soInv):
+				// if founded
+				// override res fk
+				$res[$inv['id']]['fk'][10] = $this->convertIdr($soInv['amount_untaxed'],$rate);
+				$res[$inv['id']]['fk'][11] = $this->convertIdr($soInv['amount_total'],$rate);
+
+				foreach($soInv['saleOrderLines'] as $soLine):
+					$res[$inv['id']]['of'][] = [
+						'OF',
+						(string)$soLine['product']['default_code'],#product code
+						(string)$soLine['product']['name_template'],#product name
+						$this->convertIdr($soLine['price_unit'],$rate), #HARGA SATUAN
+						(float)$soLine['product_uom_qty'], #qty
+						$this->convertIdr(($soLine['price_unit']*$soLine['product_uom_qty']),$rate), #HARGA TOTAL
+						$this->convertIdr($soLine['discount'],$rate), #DISKON
+						$this->convertIdr(($soLine['price_unit']*$soLine['product_uom_qty']),$rate), #dpp
+						$this->convertIdr((($soLine['price_unit']*$soLine['product_uom_qty'])*0.1),$rate), #ppn,
+						0, #TARIF PPNBM
+						'0.0' #ppnbm
+
+					];	
+
+				endforeach;
+			endif;
+		}else{
+			foreach($inv['accountInvoiceLines'] as $item):
+				$res[$inv['id']]['of'][] = [
+					'OF', #of
+					(string)$item['product']['default_code'],#product code
+					(string)$item['product']['name_template'],#product name
+					$this->convertIdr($item['price_unit'],$rate), #harga satuan
+					(float)$item['quantity'], #jumlah barang
+					$this->convertIdr($item['price_subtotal'],$rate), #harga total
+					$this->convertIdr($item['discount'],$rate), #diskon
+					$this->convertIdr($item['price_subtotal'],$rate), #dpp
+					$this->convertIdr(($item['price_subtotal']*0.1),$rate), #ppn,
+					'0', #ppnbm,
+					'0.0'
+
+				];
+				// echo $item['price_subtotal'].'\\';
+			endforeach;
+		}
+		return $res;
+	}
+
 }
 ?>

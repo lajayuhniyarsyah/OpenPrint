a1528d9fe8555c0868ebdef7523b77250cf8595f
ekachandra <ekachandrasetiawan@yahoo.com>
2015-07-06 11:14:59 +0700

    perbaikan Turn Over
    perbaikan Turn Over

--- views/report-accounting/turnover.php
+++ views/report-accounting/turnover.php
@@ -1,6 +1,4 @@
-	<?php
-	use yii\db\Query;
-?>
+	
 <?php
 	$jenisreport="out";
 	
@@ -10,7 +8,9 @@
 						<th>No</th>
 						<th>Type</th>
 						<th>Date</th>
+						<th>No PO</th>
 						<th>No doc</th>
+						
 						<th>No OP / LBM / PB</th>
 						<th>Qty</th>
 						<th>Source Loc</th>
@@ -38,44 +38,31 @@
 				$no_surat=substr($value['dn'], 0,7);
 				$no_pb=substr($value['op'], 0,7);
 			}
-
+			$jenis=$value['jenis'];
 			if($value['jenis']==""){
-				$jenis=$value['product_name'];
+				if($value['location_id']==5){
+					$jenis='Adjutsment';
+				}else if($value['location_id']==53){
+					$jenis='IN / Konversi';
+				}else if($value['location_id']==52){
+					$jenis='Out / Konversi';
+				}
+				// $jenis=$value['product_name'];
 			}else{
 				$jenis=$value['jenis'];
 			}
-			if($value['partner_id']){
-				$cekpartner = new Query;
-					$cekpartner
-						->select('parent_id')
-						->from('res_partner')
-						->where(['id' => $value['partner_id']]);
-					$res=$cekpartner->one();
-
-				if($res['parent_id']==""){
-					$partner=$value['partner'];
-				}else{
-					$parent = new Query;
-						$parent
-							->select('name')
-							->from('res_partner')
-							->where(['id' => $res['parent_id']]);
-						$r=$parent->one();
-
-					$partner=$r['name'];
-				}
-			}
-
 			$body[]='<tr>
 					<td>'.$no.'</td>
 					<td>'.$jenis.'</td>
 					<td>'.Yii::$app->formatter->asDatetime($value['date'], "php:d-m-Y").'</td>
+					<td>'.$value['no_po'].'</td>
 					<td>'.$no_surat.'</td>
+
 					<td>'.$no_pb.'</td>
 					<td class=right>'.floatval($qty).'</td>
 					<td>'.$value['location'].'</td>
 					<td>'.$value['desc_location'].'</td>
-					<td>'.$partner.'</td>
+					<td>'.$value['partner'].'</td>
 				 </tr>';
 		$no++;
 		}

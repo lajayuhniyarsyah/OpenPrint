5ca9b71d534ae6d1aee57acb8516e216293c8aba
ekachandra <ekachandrasetiawan@yahoo.com>
2015-08-03 15:51:57 +0700

    Perbaikan export Invoice UpperCase

--- views/res-partner/report/exportcsvtree.php
+++ views/res-partner/report/exportcsvtree.php
@@ -17,7 +17,7 @@
 
     foreach ($model as $value) {
     	$npwp=str_replace('-','', str_replace('.','', $value['npwp']));
-        $name=str_replace('"', '', $value['name']);
+        $name=str_replace('"', '', strtoupper($value['name']));
         $street=str_replace('"', '', $value['street']);
         $phone=str_replace('"', '', $value['phone']);
         $zip=str_replace('"', '', $value['zip']);

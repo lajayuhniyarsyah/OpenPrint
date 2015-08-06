d5000b9687e4cd7b5a9d5f9942d59ab7272ec1d5
Supra ERP <erpadmin@openerp.(none)>
2015-08-06 10:55:40 +0700

    MMMM

---
+++ views/attendance/first_and_last_scan.php
@@ -0,0 +1,72 @@
+<?php
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+use yii\bootstrap\Dropdown;
+?>
+<div class="form">
+	<h1>
+		Attendance Log
+		<span id="yearTitle" class="dropdown">
+			<a href="#"  data-toggle="dropdown" class="dropdown-toggle"><?=Html::encode($year)?></a>
+			<?php
+				$start = 2015;
+				$end = date('Y');
+				$items = [];
+				for($iYear=$start;$iYear<=$end;$iYear++){
+					$items[] = ['label' => $iYear, 'url' => ['','year'=>$iYear]];
+				}
+				echo Dropdown::widget([
+					'items' => $items,
+				]);
+			?>
+		</span>
+		-
+		<span id="monthTitle" class="dropdown">
+			<a href="#"  data-toggle="dropdown" class="dropdown-toggle"><?=Html::encode($month)?></a>
+			<?php
+				$items = [];
+				for($m=1;$m<=12;$m++){
+					$items[] = ['label' => $m, 'url' => ['','month'=>$m]];
+				}
+				echo Dropdown::widget([
+					'items' => $items,
+				]);
+			?>
+		</span>
+	</h1>
+	<!-- <div>
+		<?php $form = ActiveForm::begin(['id'=>'AttendaceLogForm','layout'=>'horizontal','method'=>'post']); ?>
+			
+			<?= $form->field($attendanceLogForm,'employee')?>
+			<?= $form->field($attendanceLogForm,'department')?>
+
+			<?= $form->field($attendanceLogForm,'year')?>
+			<?= $form->field($attendanceLogForm,'month')?>
+			<?= $form->field($attendanceLogForm,'day')?>
+			
+			<div class="form-group">
+	        	<div class="col-lg-offset-3 col-lg-11">
+					<?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+				</div>
+			</div>
+		<?php ActiveForm::end(); ?>
+	</div>-->
+	<?=\kartik\export\ExportMenu::widget([
+		'dataProvider'=>$dataProvider,
+
+	])?>
+	<?=\kartik\grid\GridView::widget([
+		'dataProvider'=>$dataProvider,
+		'emptyCell'=>"&nbsp;",
+		'columns'=>[
+			'year',
+			'month',
+			'day',
+			'employee',
+			'hour_1',
+			'minute_1',
+			'hour_2',
+			'minute_2',
+		]
+	])?>
+</div>

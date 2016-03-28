9709ee6d087057aae3955266332a2e3baaa5ac4a
hendri30 <hendrinursyahbani@gmail.com>
2016-03-28 17:21:53 +0700

    print out invoice on progress

--- controllers/PrintOutController.php
+++ controllers/PrintOutController.php
@@ -3,25 +3,56 @@
 namespace app\controllers;
 
 use Yii;
-use app\models\HrAttendanceMinMaxLog;
-use app\models\HrAttendanceMinMaxLogSearch;
+use app\models\AccountInvoice;
+use app\models\AccountInvoiceSearch;
+
+use app\models\OrderInvoiceReportForm;
+use app\models\ResUsers;
+use app\models\ResGroups;
+use app\models\ResGroupsUsersRel;
+use app\models\GroupSales;
+use app\models\GroupSalesLine;
+use app\models\ExecutiveSummarySales;
+use app\models\ExecutiveSummaryGroup;
+use app\models\ExecutiveSummaryGroupSearch;
+
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 
 /**
- * AttendanceController implements the CRUD actions for HrAttendanceMinMaxLog model.
+ * PrinOutController implements the CRUD actions for AccountInvoice model.
  */
 class PrintOutController extends Controller
 {
 	public function actionPrintInvoice($id,$uid=null,$printer="refa")
 	{
 		$oe = Yii::$app->openERPLib;
-
 		$login = $oe->login("admin","admin");
-		$model = $oe->read([16687],[],"account.invoice");
-		var_dump($model);
 
-		// return $this->render('account-invoice/invoice',['model'=>$model]);
+		# account invoice
+		$modelAccInvoice = $oe->read([$id],[],"account.invoice");
+		foreach ($modelAccInvoice as $keyAccInvoice => $valueAccInvoice) { }
+		
+		# res partner
+		$queryPartner = <<<query
+SELECT rp.id FROM account_invoice ai LEFT JOIN res_partner rp on rp.id = ai.partner_id WHERE ai.id = $id
+query;
+		$connection = Yii::$app->db;
+		$resPartner = $connection->createCommand($queryPartner)->queryAll();
+		$idPartner = null;
+		foreach ($resPartner as $resKeyPartner => $resValuePartner) {
+			$idPartner = $resValuePartner['id'];
+		}
+		$modelPartner = $oe->read([$idPartner],[],"res.partner");
+		foreach ($modelPartner as $keyPartner => $valuePartner) { }
+		var_dump($valuePartner);
+
+		return $this->render('account-invoice/invoice',[
+			'model'=>$valueAccInvoice,
+			'modelPartner'=>$valuePartner,
+			'printer'=>$printer,
+			'uid'=>$uid
+		]);
 	}
 }

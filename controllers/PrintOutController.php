1089850e914bc688fc3953eaf453dbd6b9337fc3
hendri30 <hendrinursyahbani@gmail.com>
2016-04-04 17:36:58 +0700

    print ount invoice, status on progress

--- controllers/PrintOutController.php
+++ controllers/PrintOutController.php
@@ -25,34 +25,231 @@ use yii\filters\VerbFilter;
  */
 class PrintOutController extends Controller
 {
+	private function prepareTotal(){
+		return [
+			'subtotal'=>0,
+			'subtotalMainCurr'=>0,
+			'discountSubtotal'=>0,
+			'discountSubtotalMainCurr'=>0,
+			'amountUntaxed'=>0,
+			'amountUntaxedMainCurr'=>0,
+			'amountTax'=>0,
+			'amountTaxMainCurr'=>0,
+			'amountTotal'=>0,
+			'amountTotalMainCurr'=>0,
+		];
+	}
+
 	public function actionPrintInvoice($id,$uid=null,$printer="refa")
 	{
+		$id = (int) $id;
+		$this->layout = 'printout';
+
 		$oe = Yii::$app->openERPLib;
-		$login = $oe->login("admin","admin");
+		$login = $oe->login("admin","supra");
 
 		# account invoice
 		$modelAccInvoice = $oe->read([$id],[],"account.invoice");
-		foreach ($modelAccInvoice as $keyAccInvoice => $valueAccInvoice) { }
-		
-		# res partner
-		$queryPartner = <<<query
-SELECT rp.id FROM account_invoice ai LEFT JOIN res_partner rp on rp.id = ai.partner_id WHERE ai.id = $id
-query;
-		$connection = Yii::$app->db;
-		$resPartner = $connection->createCommand($queryPartner)->queryAll();
-		$idPartner = null;
-		foreach ($resPartner as $resKeyPartner => $resValuePartner) {
-			$idPartner = $resValuePartner['id'];
+		// var_dump($modelAccInvoice);
+
+		if(!$modelAccInvoice){
+			throw new NotFoundHttpException('Data not found.');
 		}
+
+		$model = $modelAccInvoice[0]; // karena sudah pasti 1 data yang dicari
+
+		$idPartner = $model['partner_id'][0];
+		$idCurrency = $model['currency_id'][0];
+
+		# res partner
 		$modelPartner = $oe->read([$idPartner],[],"res.partner");
-		foreach ($modelPartner as $keyPartner => $valuePartner) { }
-		var_dump($valuePartner);
+		$valuePartner = $modelPartner[0];
+
+		# res currency
+		$modelCurrency = $oe->read([$idCurrency],[],"res.currency");
+		$valueCurrency = $modelCurrency[0];
+
+
+
+
+		$isMainCurrency = false;
+		if($idCurrency['currency_id']==13){
+			$isMainCurrency=true;
+		}
+		$invoice = [
+			'no'=>null,
+			'partner'=>null,
+			'fakturNo'=>null,
+			'currency'=>$valueCurrency['name'],
+			'rate'=>($isMainCurrency?1:$model['pajak']),
+			'comment'=>nl2br($model['comment']),
+			'lines'=>[
+				/*'no'=>null,
+				'name'=>null,
+				'priceUnit'=>0,
+				'priceUnitMainCurr'=>0,
+				'qty'=>0,
+
+				'priceSubtotal'=>0,
+				'priceSubtotalMainCurr'=>0,
+				
+				'discountPercentage'=>0,
+				'discountAmount'=>0,
+				'discountMainCurr'=>0,
+
+				'taxMainCurr'=>0,
+				'totalAmountMainCurr'=>0,*/
+			],
+			'total'=>$this->prepareTotal()
+		];
+		$lines = [];
+		$idx=0;
+		$counterLinePriced = 0; #use for count how much inv line item has been priced,, is use for counting floor method in end of result
+
+		# account invoice line
+		$accInvoiceLine = $model['invoice_line'];
+		foreach ($accInvoiceLine as $keyInvoiceLine => $valueInvoiceLine) {
+			$idAccInvoiceLine = $valueInvoiceLine.',';
+			$modelAccInvoiceLine = $oe->read([$idAccInvoiceLine],[],"account.invoice.line");
+			foreach ($modelAccInvoiceLine as $keyAccInvoiceLine => $valueAccInvoiceLine) {
+
+				$priceUnitMainCurr = round($valueAccInvoiceLine['price_unit'] * $invoice['rate'],2);
+				if($valueAccInvoiceLine['price_unit']>0){
+					$counterLinePriced++;
+				}
+				$priceSubtotal = ($isMainCurrency ? round($valueAccInvoiceLine['quantity']*$valueAccInvoiceLine['price_unit'],2):($valueAccInvoiceLine['quantity']*$valueAccInvoiceLine['price_unit']));
+				$priceSubtotalMainCurr = round($priceUnitMainCurr*$valueAccInvoiceLine['quantity'],2);
+
+				if($isMainCurrency){
+					// IF MAIN CURRENCY
+					$discountMainCurr = $valueAccInvoiceLine['amount_discount'];
+				}
+				else{
+					// IF VALAS
+					// discount filled, amount discount filled
+					// OR
+					// discount filled, amount discount not filled
+					if(($valueAccInvoiceLine['discount']>0 && $valueAccInvoiceLine['amount_discount']) || ($valueAccInvoiceLine['discount'] && !$valueAccInvoiceLine['amount_discount'])){
+						// THEN MAIN CURR WILL TAKE PERCENTAGE OF MAIN CURRENCY
+						$discountMainCurr = round($priceSubtotalMainCurr*($valueAccInvoiceLine['discount']/100),2);
+					}
+					// discount not filled amoount discount filled
+					elseif($valueAccInvoiceLine['discount']<=0 && $valueAccInvoiceLine['amount_discount']){
+
+						$discountMainCurr = round($valueAccInvoiceLine['amount_discount']*$invoice['rate'],2);
+					}
+					// disocunt not filled, amount discount not filled
+					else{
+						$discountMainCurr = 0;
+					}
+				}
+
+				$priceTotal = ($valueAccInvoiceLine['price_unit']*$valueAccInvoiceLine['quantity'])-$valueAccInvoiceLine['amount_discount'];
+				$priceTotalMainCurr = round($priceSubtotalMainCurr-$discountMainCurr,2);
+
+				$data['lines'][] = [
+					'id'=>$valueAccInvoiceLine['id'],
+
+		            'no'=>$valueAccInvoiceLine['sequence'],
+		            'name'=>$valueAccInvoiceLine['name'].'<br><br>',
+
+		            'priceUnit'=>$valueAccInvoiceLine['price_unit'],
+		            'priceUnitMainCurr'=>'&nbsp;',
+		            'qty'=>$valueAccInvoiceLine['quantity'].'.00 pcs',
+		            'unit'=>'&nbsp;',
+
+		            'priceSubtotal'=>'&nbsp;',
+		            'priceSubtotalMainCurr'=>'&nbsp;',
+		            
+		            'discountPercentage'=>$valueAccInvoiceLine['discount'],
+		            'discountAmount'=>$valueAccInvoiceLine['amount_discount'],
+		            'discountMainCurr'=>'&nbsp;',
+
+		            'priceTotal'=>'&nbsp;',
+		            'priceTotalMainCurr'=>'&nbsp;',
+
+		            'taxMainCurr'=>'&nbsp;',
+		            'totalAmountMainCurr'=>'&nbsp;',
+
+		            'formated'=>[
+		                'currency'=>$valueCurrency['name'],
+		                'priceUnit'=>$valueAccInvoiceLine['price_unit'],
+		                'priceUnitMainCurr'=>'&nbsp;',
+		                // 'qty'=>$invLine->quantity,
+
+		                'priceSubtotal'=>round($valueAccInvoiceLine['quantity']*$valueAccInvoiceLine['price_unit'],2),
+		                'priceSubtotalMainCurr'=>'&nbsp;',
+		                
+		                'discountPercentage'=>'&nbsp;',
+		                'discountAmount'=>'&nbsp;',
+		                'discountMainCurr'=>'&nbsp;',
+
+		                'priceTotal'=>'&nbsp;',
+		                'priceTotalMainCurr'=>'&nbsp;',
+
+		                'taxMainCurr'=>'&nbsp;',
+		                'totalAmountMainCurr'=>'&nbsp;',
+		            ]
+				];
+			}
+		}
+
+		$data['lines'][] = [
+			'id'=>'notes',
+
+            'no'=>'&nbsp;',
+            'name'=>'PO NO : '.$model['name'].'<br/>'.nl2br($model['comment']),
+
+            'priceUnit'=>'&nbsp;',
+            'priceUnitMainCurr'=>'&nbsp;',
+            'qty'=>'&nbsp;',
+            'unit'=>'&nbsp;',
+
+            'priceSubtotal'=>'&nbsp;',
+            'priceSubtotalMainCurr'=>'&nbsp;',
+            
+            'discountPercentage'=>'&nbsp;',
+            'discountAmount'=>'&nbsp;',
+            'discountMainCurr'=>'&nbsp;',
+
+            'priceTotal'=>'&nbsp;',
+            'priceTotalMainCurr'=>'&nbsp;',
+
+            'taxMainCurr'=>'&nbsp;',
+            'totalAmountMainCurr'=>'&nbsp;',
+
+            'formated'=>[
+                'currency'=>'&nbsp;',
+                'priceUnit'=>'&nbsp;',
+                'priceUnitMainCurr'=>'&nbsp;',
+                // 'qty'=>'&nbsp;',
+
+                'priceSubtotal'=>'&nbsp;',
+                'priceSubtotalMainCurr'=>'&nbsp;',
+                
+                'discountPercentage'=>'&nbsp;',
+                'discountAmount'=>'&nbsp;',
+                'discountMainCurr'=>'&nbsp;',
+
+                'priceTotal'=>'&nbsp;',
+                'priceTotalMainCurr'=>'&nbsp;',
+
+                'taxMainCurr'=>'&nbsp;',
+                'totalAmountMainCurr'=>'&nbsp;',
+            ]
+		];
+
+		if($printer == null && ($uid==100 || $uid == 191)){
+			$printer='sri';
+		}
 
 		return $this->render('account-invoice/invoice',[
-			'model'=>$valueAccInvoice,
+			'model'=>$model,
 			'modelPartner'=>$valuePartner,
+			'modelCurrency'=>$valueCurrency,
 			'printer'=>$printer,
-			'uid'=>$uid
+			'uid'=>$uid,
+			'data'=>$data,
 		]);
 	}
 }
--- views/print-out/account-invoice/invoice.php
+++ views/print-out/account-invoice/invoice.php
@@ -253,7 +253,7 @@ use yii\helpers\Url;
 	
 	$this->registerCss($css);
 	$formated = function($value) use ($model){
-		if($model->currency_id==13){
+		if($model['currency_id']==13){
 			return Yii::$app->numericLib->indoStyle(floatval($value));
 		}else{
 			return Yii::$app->numericLib->westStyle(floatval($value));
@@ -262,7 +262,7 @@ use yii\helpers\Url;
 ?>
 <div class="choosePrinter">
 	<form method="get" id="formSelectPrinter">
-		<input type="hidden" value="<?=Url::to('account-invoice/print-invoice')?>" name="r" />
+		<input type="hidden" value="<?=Url::to('print-out/print-invoice')?>" name="r" />
 		<input type="hidden" value="<?=$model['id']?>" name="id" />
 		<input type="hidden" value="<?=Yii::$app->request->get('uid')?>" name="uid" />
 		Print To : 
@@ -281,6 +281,236 @@ use yii\helpers\Url;
 				<div class="partnerAddr2" contenteditable="true"><?=$modelPartner['street2']?></div>
 				<div class="partnerAddr2" contenteditable="true"><?=$modelPartner['city']?></div>
 			</div>
+			<div class="rightInfo">
+				<div class="kwNo"><?=$model['kwitansi']?></div>
+				<div class="dateInv" contenteditable="true"><?=Yii::$app->formatter->asDatetime($model['date_invoice'], "php:d-m-Y")?></div>
+			</div>
+			<div class="clear">&nbsp;</div>
 		</div>
+		<div class="superContain" style="max-height:117mm;">
+			<div class="containerLines">
+				<table class="contentLines">
+					<!-- <tr></tr> -->
+				</table>
+			</div>
+		</div>
+		<?php /* 
+		<div class="footers">
+			<div class="amounts">
+				<div class="amLine1 am"><?='<div class="currSymbol">'.$modelCurrency['name'].'</div><div class="amountNumber">'.$formated(($model['currency_id']==13 ? $data['total']['subtotalMainCurr']:$data['total']['subtotal'])).'</div><div class="clear"></div>'?></div>
+				<div class="amLine2 am">
+					<div class="am2Words" contenteditable="true"><?=($data['total']['discountSubtotal'] ? 'DISCOUNT':'&nbsp;')?></div>
+					<div class="kursAm2" contenteditable="true"><?=($data['total']['discountSubtotal'] ? $data['currency']:'&nbsp;')?></div>
+					<div class="valAm2" contenteditable="true"><?=($data['total']['discountSubtotal'] ? ($model['currency_id']==13?$formated($data['total']['discountSubtotalMainCurr']):$formated($data['total']['discountSubtotal'])):'&nbsp;')?></div>
+				</div>
+				<div class="amLine3 am"><?='<div class="currSymbol">'.$modelCurrency['name'].'</div><div class="amountNumber">'.$formated(($model['currency_id']==13 ? $data['total']['amountUntaxedMainCurr']:$model['amount_untaxed'])).'</div><div class="clear"></div>'?></div>
+				<div class="amLine4 am"><?='<div class="currSymbol">'.$modelCurrency['name'].'</div><div class="amountNumber">'.$formated(($model['currency_id']==13 ? $data['total']['amountTaxMainCurr']:$model['amount_tax'])).'</div><div class="clear"></div>'?></div>
+				<div class="amLine5 am"><?='<div class="currSymbol">'.$modelCurrency['name'].'</div><div class="amountNumber">'.$formated(($model['currency_id']==13 ? $data['total']['amountTotalMainCurrCounted']:$model['amount_total'])).'</div><div class="clear"></div>'?></div>
+			</div>
+			<div class="notes">
+				<div class="terb" contenteditable="true">
+					<?php
+					
+					switch (trim($modelCurrency['name'])) {
+						case 'USD':
+							# code...
+							$preCur = '# United State Dollar ';
+							$subCur = "#";
+							break;
+						case 'SGD':
+							$preCur = '# Singapore Dollar ';
+							$subCur = "#";
+							break;
+						case 'EUR':
+							$preCur = '# EURO';
+							$subCur = "#";
+							break;
+						case 'IDR':
+							$preCur = '#';
+							$subCur = " Rupiah #";
+							break;
+						default:
+							# code...
+							$preCur='#';
+							$subCur = "#";
+							break;
+					}
+					echo $preCur;
+					?>
+					<?=ucwords(Yii::$app->numericLib->convertToWords($model['amount_total'],$modelCurrency['name']))?>
+					<?=$subCur?>
+
+				</div>
+				<div class="dueDate"><?=(isset($model->paymentTerm->name) ? $model->paymentTerm->name:"")?></div>
+				<div class="invFootNotes">
+					Bank Mandiri Cab. Ketapang Indah, Jakarta -> A/C : 115-000-122-6655 (IDR)
+					<br>
+					Bank Mandiri Cab. Sunter Mall, Jakarta -> A/C : 120-000-669-0205 (USD)
+					<br/>
+					Bank Mandiri Cab. Sunter Mall, Jakarta -> A/C : 120-000-991-1988 (EUR)
+					<br/>
+					Bank CIMB Niaga Cab. Wahid Hasyim, Jakarta -> A/C : 4230-3000-02-008 (AUD)
+				</div>
+				<div class="signName" contenteditable="true"><?=strtoupper($model->approver0->partner->name)?></div>
+			</div>
+		</div>
+		*/ ?>
 	</div>
 </div>
+
+<?php
+$jsonData = \yii\helpers\Json::encode($data);
+// $jsonLines = \yii\helpers\Json::encode($lines);
+$jsonModel = \yii\helpers\Json::encode($model);
+// var_dump($jsonModel);
+// var_dump($data);
+$scr = <<<EOD
+var currPage = 1;
+
+// save page template to var
+var tmpl = jQuery('div#container').html();
+
+// add id to container
+jQuery('div.pages').attr('id','page'+currPage);
+jQuery('table.contentLines:last').attr('id','lines'+currPage);
+jQuery('.containerLines:last').attr('id','tdLine'+currPage);
+
+
+// data to render
+var data = $jsonData
+// var lines = \$jsonLines;
+var lines = data.lines;
+var model = $jsonModel;
+var maxLinesHeight = jQuery('.containerLines:last').height();
+
+
+var currRow = 0;
+
+console.log(maxLinesHeight);
+
+function prepareRow(rowNo,data)
+{
+	return "<tr id='lines"+data.id+"' class='cRows rows"+rowNo+"'><td class='td1' contenteditable='true'>"+data.no+"</td><td class='td2'>"+data.qty+"&nbsp;"+data.unit+"</td><td class='td3'>"+data.name+"</td><td class='td4'>"+(data.formated.priceUnit ? data.formated.currency+"&nbsp;":"")+data.formated.priceUnit+"</td><td class='td5'>"+data.formated.currency+"&nbsp;"+data.formated.priceSubtotal+"</td></tr>";
+}
+
+function getNotes(notes,rowNo=999999)
+{
+	return "<tr class='cRows rows"+rowNo+"'><td style='width:23%;'></td><td style='width:57%;padding-top:10mm;'>Notes : <br/>"+notes+"</td><td></td></tr>";
+}
+var rowPage = 0;
+
+jQuery.each(lines,function(key,line){
+	var getRow = prepareRow(currRow,line);
+	if(key==0)
+	{
+		jQuery('#lines'+currPage).html(getRow);
+	}
+	else
+	{
+		jQuery('#lines'+currPage+' tr:last').after(getRow);
+	}
+	rowPage = rowPage+1;
+
+	var currLineHeight = jQuery('#tdLine'+currPage).height();
+	if(currLineHeight>maxLinesHeight){
+		// remove last row
+		jQuery('#lines'+currPage+' tr:last').remove();
+		// add new page container
+		jQuery('div#page'+currPage).after(tmpl);
+		console.log('div#page'+currPage);
+		currPage = currPage+1;
+		console.log(currPage);
+		// add id to new div
+		jQuery('div.pages:last').attr('id','page'+currPage);
+		jQuery('.contentLines:last').attr('id','lines'+currPage);
+		jQuery('.containerLines:last').attr('id','tdLine'+currPage);
+
+		jQuery('#lines'+currPage).html(getRow);
+		currLineHeight = jQuery('#tdLine'+currPage).height();
+		// jQuery('.pager:last').html(currPage);
+		// console.log(tmpl);
+		
+	}
+	
+	console.log('Rendering Page '+currPage+' Row '+currRow+' Height => '+currLineHeight);
+	currRow=currRow+1;
+});
+// end loop
+
+var currIndex = 0;
+function refreshActButton(currIndex){
+	jQuery('.btnActLine').remove();
+	jQuery('.td5').each(function(ro,v){
+		jQuery(this).append('<div class="btnActLine hideOnPrint" style="margin-left: 150px;position: absolute;"><a href="#" class="btnCutRow" data="'+ro+'">Cut</a><a href="#" data="'+ro+'" class="btnPaste btnPasteBefore hidden"> | Paste Before</a><a href="#" class="btnPaste btnPasteAfter hidden" data="'+ro+'"> | Paste After</a></div>');
+		currIndex = currIndex+1;
+		return currIndex;
+	});
+}
+
+currIndex = refreshActButton();
+
+var trCopy = "";
+var trCopyIdx;
+/*jQuery('.btnCutRow').click(function(){
+	trCopy = jQuery(this).parents("tr").html();
+	trCopyIdx = jQuery(this).attr('data');
+	console.log(trCopy);
+	jQuery('.btnPaste').show();
+	return false;
+});
+
+jQuery('.btnPasteAfter').click(function(){
+	var roNo = jQuery(this).attr('data');
+	jQuery("<tr>"+trCopy+"</tr>").insertAfter('tr:eq('+roNo+')');
+	jQuery("tr:eq("+trCopyIdx+")").remove();
+	trCopy = "";
+	trCopyIdx = "";
+	jQuery('.btnPaste').hide();
+	return false;
+});*/
+
+jQuery('#container').on('click','.btnCutRow',function(e){
+	e.preventDefault();
+	var target = jQuery(this).parents("tr");
+	jQuery(this).parent().remove();
+	trCopy = target.html();
+	trCopyIdx = jQuery(this).attr('data');
+	console.log("THtml = "+trCopy+". Index = "+trCopyIdx);
+	jQuery('.btnPaste').show();
+	console.log("Removing TR "+trCopyIdx);
+	jQuery("tr.rows"+trCopyIdx).remove();
+	return false;
+});
+
+jQuery('#container').on('click','.btnPasteAfter',function(e){
+	e.preventDefault();
+	var roNo = jQuery(this).attr('data');
+	
+	
+	console.log("inserting after "+roNo);
+	currIndex = currIndex+1;
+	jQuery('<tr class="cRows rows'+currIndex+'"></tr>'+trCopy+"</tr>").insertAfter('tr.rows'+roNo);
+	console.log(jQuery('tr.rows'+currIndex+' .btnCutRow').attr('class'));
+	trCopy = "";
+	trCopyIdx = "";
+	jQuery('.btnPaste').hide();
+	return false;
+});
+
+jQuery('#container').on('click','.btnPasteBefore',function(e){
+	e.preventDefault();
+	var roNo = jQuery(this).attr('data');
+	console.log("inserting before "+roNo);
+	currIndex = currIndex+1;
+	jQuery('<tr class="cRows rows'+currIndex+'"></tr>'+trCopy+"</tr>").insertBefore('tr.rows'+roNo);
+	console.log(jQuery('tr.rows'+currIndex+' .btnCutRow').attr('class'));
+	trCopy = "";
+	trCopyIdx = "";
+	jQuery('.btnPaste').hide();
+	return false;
+});
+jQuery('tr#linesnotes td').attr('contenteditable','true');
+EOD;
+unset($jsonLines);
+$this->registerJs($scr);
+?>

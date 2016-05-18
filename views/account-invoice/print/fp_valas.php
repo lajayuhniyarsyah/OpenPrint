e5787f0c7aa0a8428e0c0d8ac0e36ff1828426cd
Server www <lajayuhni@gmail.com>
2016-05-18 09:03:00 +0700

    Fixing partnerName bug issue

--- views/account-invoice/print/fp_valas.php
+++ views/account-invoice/print/fp_valas.php
@@ -199,7 +199,7 @@ use yii\helpers\Url;
                                             }
                                             
                                         }else{
-                                            $partnerName = $partnerName;
+                                            $partnerName = $prtName;
                                         }
                                         
                                         echo $partnerName;
